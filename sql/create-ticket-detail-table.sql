72c320edb3aa661a9e3e4375e1bdd0e07cfe60c7
Yoshiaki Iinuma <iinumayoshiaki@gmail.com>
2019-07-09 11:54:45 -1000

    Fixed TicketDetails table timestamp field names

--- sql/create-ticket-detail-table.sql
+++ sql/create-ticket-detail-table.sql
@@ -3,12 +3,12 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='TicketDetails' AND xtype='U'
   (
     id int NOT NULL UNIQUE,
     ticketId int NOT NULL FOREIGN KEY REFERENCES Tickets(id),
-    detailNotes ntext,
     title nvarchar(1024),
     timeSpent int,
-    createdBy nvarchar(128),
-    createdAt datetime,
-    modifiedBy nvarchar(128),
-    modifiedAt datetime,
+    detailNotes nvarchar(max),
+    createdBy nvarchar(256),
+    created datetime,
+    modifiedBy nvarchar(256),
+    modified datetime,
     CONSTRAINT PK_ticket_details PRIMARY KEY CLUSTERED (id)
   )
