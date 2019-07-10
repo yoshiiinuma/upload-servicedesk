3defc1da50782b20ae01e6e6b362f560b4ea04f1
Yoshiaki Iinuma <iinumayoshiaki@gmail.com>
2019-07-09 16:33:42 -1000

    Changed upload-tickets and upload-details to accept arguments

--- src/upload-details.js
+++ src/upload-details.js
@@ -1,11 +1,48 @@
 
+import fs from 'fs';
 import config from '../config/config.js';
 import { getDetailUploader } from './uploader.js';
 
-const file = 'ServiceDeskAdminDetails.csv';
-const PER = 1000; // Send a BulkLoad Request per this number
-//const PER = 5; // Send a BulkLoad Request per this number
+const usage = () => {
+  console.log("USAGE: npm run upload-details -- <CSV> [<DATA-PER-REQ>]");
+  console.log("USAGE: node dist/upload-details.js <CSV> [<DATA-PER-REQ>]");
+  console.log("");
+  console.log("   CSV: File contains Ticket detail data");
+  console.log("   DATA-PER-REQ: Specifies the number of records that a BulkLoad sends in a request (default 1000)");
+  console.log("");
+  console.log("   OPTIONS:");
+  console.log("   -h or --help:             Shows this usage");
+  console.log("");
+}
+
+if (process.argv.length < 3 || process.argv.length > 4) {
+  usage();
+  process.exit();
+}
+
+//const file = 'ServiceDeskAdminDetails.csv';
+const file = process.argv[2];
+
+if (!fs.existsSync(file)) {
+  console.log('CSV Not Found: ' + file + "\n");
+  usage();
+  process.exit();
+}
+
+const rgxNum = /^\d+$/;
+let dpr = 1000;  //Data Per Request
+
+if (process.argv.length == 4) {
+  dpr = process.argv[3];
+  if (!rgxNum.test(dpr)) {
+    console.log('DATA-PER-REQ Not Number: ' + dpr + "\n");
+    usage();
+    process.exit();
+  }
+  dpr = parseInt(dpr);
+}
+
 
 const uploader = getDetailUploader(config);
-uploader.uploadCsv(file, PER);
+uploader.uploadCsv(file, dpr);
 
--- src/upload-tickets.js
+++ src/upload-tickets.js
@@ -1,14 +1,49 @@
 
+import fs from 'fs';
 import config from '../config/config.js';
-//import * as TicketParser from './csv-ticket-parser.js';
 import { getTicketUploader } from './uploader.js';
 
-const file = 'ServiceDeskAdmin.csv';
+const usage = () => {
+  console.log("USAGE: npm run upload-tickets -- <CSV> [<DATA-PER-REQ>]");
+  console.log("USAGE: node dist/upload-tickets.js <CSV> [<DATA-PER-REQ>]");
+  console.log("");
+  console.log("   CSV: File contains Ticket data");
+  console.log("   DATA-PER-REQ: Specifies the number of records that a BulkLoad sends in a request (default 1000)");
+  console.log("");
+  console.log("   OPTIONS:");
+  console.log("   -h or --help:             Shows this usage");
+  console.log("");
+}
+
+if (process.argv.length < 3 || process.argv.length > 4) {
+  usage();
+  process.exit();
+}
+
 //const file = 'ServiceDeskAdmin-small.csv';
-const PER = 1000; // Send a BulkLoad Request per this number
-//const PER = 5; // Send a BulkLoad Request per this number
+//const file = 'ServiceDeskAdmin.csv';
+const file = process.argv[2];
+
+if (!fs.existsSync(file)) {
+  console.log('CSV Not Found: ' + file + "\n");
+  usage();
+  process.exit();
+}
+
+const rgxNum = /^\d+$/;
+let dpr = 1000;  //Data Per Request
+
+if (process.argv.length == 4) {
+  dpr = process.argv[3];
+  if (!rgxNum.test(dpr)) {
+    console.log('DATA-PER-REQ Not Number: ' + dpr + "\n");
+    usage();
+    process.exit();
+  }
+  dpr = parseInt(dpr);
+}
 
 //const uploader = getUploader(config, TicketParser);
 const uploader = getTicketUploader(config);
-uploader.uploadCsv(file, PER);
+uploader.uploadCsv(file, dpr);
 
